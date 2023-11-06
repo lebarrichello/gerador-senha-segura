89a7eed23bbf9aa7ee885ba8cdc4ff61f4d81184
lebarrichello <leticia.barrichello@gmail.com>
2023-11-06 16:14:09 -0300

    :lipstick: feat: ajustes css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import '../src/style.css';
 
 const passwordBtnEl = document.querySelector('button');
@@ -7,4 +8,9 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', () => {
+    copy(event.target.innerHTML);
+    alert('Senha copiada!');
 });
